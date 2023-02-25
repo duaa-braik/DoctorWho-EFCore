6675e9b877cf3ef359496875718d0c311c38c190
duaa-braik <duaa13braik@gmail.com>
2023-02-25 20:19:36 +0200

    Add Keyless entity for the view

---
+++ DoctorWho/Views.cs
@@ -0,0 +1,17 @@
+﻿using DoctorWho.Db.DataModels;
+using DoctorWho.Db.DBContext;
+
+namespace DoctorWho
+{
+    public class Views
+    {
+        public List<ViewEpisodes> ViewEpisodes()
+        {
+            using var context = new DoctorWhoCoreDbContext();
+
+            var Episodes = context.ViewEpisodes.ToList();
+
+            return Episodes;
+        }
+    }
+}
