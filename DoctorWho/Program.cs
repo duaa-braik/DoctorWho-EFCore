d4e5dbb52dc3c76d55a4d912c8e09571ce041256
duaa-braik <duaa13braik@gmail.com>
2023-02-25 21:04:35 +0200

    Execute the View in console app

--- DoctorWho/Program.cs
+++ DoctorWho/Program.cs
@@ -1,14 +1,18 @@
 ﻿using DoctorWho;
+using DoctorWho.Db.DataModels;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         var UserDefinedFunctions = new UserDefinedFunctions();
+        var Views = new Views();
 
         int EpisodeId = 3;
 
-        Console.WriteLine(UserDefinedFunctions.GetCompanions(EpisodeId));
-        Console.WriteLine(UserDefinedFunctions.GetEnemies(EpisodeId));
+        UserDefinedFunctions.GetCompanions(EpisodeId);
+        UserDefinedFunctions.GetEnemies(EpisodeId);
+
+        Views.ViewEpisodes();
     }
 }
